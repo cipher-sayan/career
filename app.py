0d182f37d0c8293986125adece895f4aff2bb4ea
cipher-sayan <155264194+cipher-sayan@users.noreply.github.com>
2024-11-08 21:22:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the model
-with open('D:\EXTRA DOWNLOADS\hackathon\juniors\model\.venv\mlp_model.pkl', 'rb') as f:
+with open('mlp_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Define the preprocessing function
